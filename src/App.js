b7da31f5bed0f1201c0aeb17efd7f1c40ffec0c4
Gorresubbarao <gsubbu2109@gmail.com>
2024-09-28 09:29:19 +0000

    rjs demo

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import Header from './components/Header'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import NotFound from './components/NotFound'
+
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/abt" component={About} />
+      <Route exact path="/cnt" component={Contact} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default App
+
+// to use the React Router we should install this third party package that 
+// npm install react-router-dom --save
